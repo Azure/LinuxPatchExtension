cf38e09503850b6e2b521db33ccb9dcf0c3ed4a3
Rajasi Rane <56841542+rane-rajasi@users.noreply.github.com>
2020-04-28 13:32:02 -0700

    Adding unittests for StatusHandler (#20)
    * Adding unittests for StatusHandler
    
    * PR feedback changes

--- src/core/src/service_interfaces/StatusHandler.py
+++ src/core/src/service_interfaces/StatusHandler.py
@@ -143,7 +143,7 @@ class StatusHandler(object):
         # Persisting new reboot status (with machine state incorporation)
         self.composite_logger.log_debug("Setting new installation reboot status. [NewRebootStatus={0}] [CurrentRebootStatus={1}]".format(str(new_reboot_status), self.__installation_reboot_status))
         self.__installation_reboot_status = new_reboot_status
-        self.__write_status_file()
+        self.set_installation_substatus_json()
 
     def __refresh_installation_reboot_status(self):
         """ Discovers if the system needs a reboot. Never allows going back to NotNeeded (deliberate). ONLY called internally. """
@@ -164,7 +164,7 @@ class StatusHandler(object):
     # region - Substatus generation
     def set_maintenance_window_exceeded(self, maintenance_windows_exceeded):
         self.__maintenance_window_exceeded = maintenance_windows_exceeded
-        self.__write_status_file()
+        self.set_installation_substatus_json()
 
     def set_assessment_substatus_json(self, status=Constants.STATUS_TRANSITIONING, code=0):
         """ Prepare the assessment substatus json including the message containing assessment summary """
--- src/core/tests/TestStatusHandler.py
+++ src/core/tests/TestStatusHandler.py
@@ -1,4 +1,6 @@
+import json
 import unittest
+from src.bootstrap.Constants import Constants
 from tests.library.ArgumentComposer import ArgumentComposer
 from tests.library.RuntimeCompositor import RuntimeCompositor
 
@@ -11,8 +13,104 @@ class TestStatusHandler(unittest.TestCase):
     def tearDown(self):
         self.runtime.stop()
 
-    def test_template(self):
-        self.assertEqual(True, True)
+    def test_set_package_assessment_status(self):
+        packages, package_versions = self.runtime.package_manager.get_all_updates()
+        self.runtime.status_handler.set_package_assessment_status(packages, package_versions)
+        substatus_file_data = []
+        with self.runtime.env_layer.file_system.open(self.runtime.execution_config.status_file_path, 'r') as file_handle:
+            substatus_file_data = json.load(file_handle)[0]["status"]["substatus"][0]
+        self.assertEqual(substatus_file_data["name"], Constants.PATCH_ASSESSMENT_SUMMARY)
+        self.assertEqual(len(json.loads(substatus_file_data["formattedMessage"]["message"])["patches"]), 3)
+        self.assertEqual(json.loads(substatus_file_data["formattedMessage"]["message"])["patches"][0]["name"], "python-samba")
+        self.assertEqual(json.loads(substatus_file_data["formattedMessage"]["message"])["patches"][1]["name"], "samba-common-bin")
+        self.assertEqual(json.loads(substatus_file_data["formattedMessage"]["message"])["patches"][2]["name"], "samba-libs")
+        self.assertTrue("python-samba_2:4.4.5+dfsg-2ubuntu5.4" in str(json.loads(substatus_file_data["formattedMessage"]["message"])["patches"][0]["patchId"]))
+
+    def test_set_package_install_status(self):
+        packages, package_versions = self.runtime.package_manager.get_all_updates()
+        self.runtime.status_handler.set_package_install_status(packages, package_versions)
+        substatus_file_data = []
+        with self.runtime.env_layer.file_system.open(self.runtime.execution_config.status_file_path, 'r') as file_handle:
+            substatus_file_data = json.load(file_handle)[0]["status"]["substatus"][0]
+        self.assertEqual(substatus_file_data["name"], Constants.PATCH_INSTALLATION_SUMMARY)
+        self.assertEqual(len(json.loads(substatus_file_data["formattedMessage"]["message"])["patches"]), 3)
+        self.assertEqual(json.loads(substatus_file_data["formattedMessage"]["message"])["patches"][0]["name"], "python-samba")
+        self.assertEqual(json.loads(substatus_file_data["formattedMessage"]["message"])["patches"][1]["name"], "samba-common-bin")
+        self.assertEqual(json.loads(substatus_file_data["formattedMessage"]["message"])["patches"][2]["name"], "samba-libs")
+        self.assertTrue("python-samba_2:4.4.5+dfsg-2ubuntu5.4" in str(json.loads(substatus_file_data["formattedMessage"]["message"])["patches"][0]["patchId"]))
+        self.assertEqual(json.loads(substatus_file_data["formattedMessage"]["message"])["patches"][2]["patchInstallationState"], "Pending")
+
+    def test_set_installation_reboot_status(self):
+        self.assertRaises(Exception, self.runtime.status_handler.set_installation_reboot_status, "INVALID_STATUS")
+
+        # Reboot status not updated as it fails state transition validation
+        self.runtime.status_handler.set_installation_substatus_json()
+        self.runtime.status_handler.set_installation_reboot_status(Constants.RebootStatus.COMPLETED)
+        with self.runtime.env_layer.file_system.open(self.runtime.execution_config.status_file_path, 'r') as file_handle:
+            substatus_file_data = json.load(file_handle)[0]["status"]["substatus"][0]
+        self.assertEqual(json.loads(substatus_file_data["formattedMessage"]["message"])["rebootStatus"], Constants.RebootStatus.NOT_NEEDED)
+
+        self.runtime.status_handler.set_installation_reboot_status(Constants.RebootStatus.REQUIRED)
+        with self.runtime.env_layer.file_system.open(self.runtime.execution_config.status_file_path, 'r') as file_handle:
+            substatus_file_data = json.load(file_handle)[0]["status"]["substatus"][0]
+        self.assertEqual(json.loads(substatus_file_data["formattedMessage"]["message"])["rebootStatus"], Constants.RebootStatus.REQUIRED)
+
+    def test_set_maintenance_window_exceeded(self):
+        self.runtime.status_handler.set_installation_substatus_json()
+        self.runtime.status_handler.set_maintenance_window_exceeded(True)
+        with self.runtime.env_layer.file_system.open(self.runtime.execution_config.status_file_path, 'r') as file_handle:
+            substatus_file_data = json.load(file_handle)[0]["status"]["substatus"][0]
+        self.assertTrue(json.loads(substatus_file_data["formattedMessage"]["message"])["maintenanceWindowExceeded"])
+
+    def test_add_error(self):
+        # Setting operation to assessment to add all errors under assessment substatus
+        self.runtime.status_handler.set_current_operation(Constants.ASSESSMENT)
+
+        # Unexpected input
+        self.runtime.status_handler.add_error_to_status(None)
+        self.runtime.status_handler.set_assessment_substatus_json()
+        substatus_file_data = []
+        with self.runtime.env_layer.file_system.open(self.runtime.execution_config.status_file_path, 'r') as file_handle:
+            substatus_file_data = json.load(file_handle)[0]["status"]["substatus"][0]
+        self.assertEqual(len(json.loads(substatus_file_data["formattedMessage"]["message"])["errors"]["details"]), 0)
+
+        # Adding multiple exceptions
+        self.runtime.status_handler.add_error_to_status("exception1", Constants.PatchOperationErrorCodes.DEFAULT_ERROR)
+        self.runtime.status_handler.add_error_to_status("exception2", Constants.PatchOperationErrorCodes.DEFAULT_ERROR)
+        self.runtime.status_handler.add_error_to_status("exception3", Constants.PatchOperationErrorCodes.DEFAULT_ERROR)
+        self.runtime.status_handler.add_error_to_status("exception4", Constants.PatchOperationErrorCodes.DEFAULT_ERROR)
+        self.runtime.status_handler.add_error_to_status("exception5", Constants.PatchOperationErrorCodes.DEFAULT_ERROR)
+        self.runtime.status_handler.add_error_to_status("exception6", Constants.PatchOperationErrorCodes.OPERATION_FAILED)
+
+        # Trying to add existing error
+        self.runtime.status_handler.add_error_to_status("Adding same exception " + Constants.ERROR_ADDED_TO_STATUS, Constants.PatchOperationErrorCodes.DEFAULT_ERROR)
+
+        # Test message restrictions
+        self.runtime.status_handler.add_error_to_status("a"*130, Constants.PatchOperationErrorCodes.DEFAULT_ERROR)
+
+        substatus_file_data = []
+        with self.runtime.env_layer.file_system.open(self.runtime.execution_config.status_file_path, 'r') as file_handle:
+            substatus_file_data = json.load(file_handle)[0]["status"]["substatus"][0]
+
+        self.assertNotEqual(json.loads(substatus_file_data["formattedMessage"]["message"])["errors"], None)
+        self.assertTrue("Adding same exception" not in str(json.loads(substatus_file_data["formattedMessage"]["message"])["errors"]["details"]))
+        self.assertEqual(substatus_file_data["name"], Constants.PATCH_ASSESSMENT_SUMMARY)
+        self.assertEqual(json.loads(substatus_file_data["formattedMessage"]["message"])["errors"]["code"], 1)
+        self.assertEqual(len(json.loads(substatus_file_data["formattedMessage"]["message"])["errors"]["details"]), 5)
+        self.assertEqual(json.loads(substatus_file_data["formattedMessage"]["message"])["errors"]["details"][1]["code"], Constants.PatchOperationErrorCodes.OPERATION_FAILED)
+        self.assertEqual(json.loads(substatus_file_data["formattedMessage"]["message"])["errors"]["details"][0]["code"], Constants.PatchOperationErrorCodes.DEFAULT_ERROR)
+        self.assertEqual(json.loads(substatus_file_data["formattedMessage"]["message"])["errors"]["details"][0]["message"], "a"*125 + "...")
+
+        # Adding installation error
+        self.runtime.status_handler.set_current_operation(Constants.INSTALLATION)
+        self.runtime.status_handler.add_error_to_status("installexception1", Constants.PatchOperationErrorCodes.DEFAULT_ERROR)
+        substatus_file_data = []
+        with self.runtime.env_layer.file_system.open(self.runtime.execution_config.status_file_path, 'r') as file_handle:
+            substatus_file_data = json.load(file_handle)[0]["status"]["substatus"][1]
+        self.assertNotEqual(json.loads(substatus_file_data["formattedMessage"]["message"])["errors"], None)
+        self.assertEqual(substatus_file_data["name"], Constants.PATCH_INSTALLATION_SUMMARY)
+        self.assertEqual(json.loads(substatus_file_data["formattedMessage"]["message"])["errors"]["code"], 1)
+        self.assertEqual(len(json.loads(substatus_file_data["formattedMessage"]["message"])["errors"]["details"]), 1)
 
 
 if __name__ == '__main__':
