5ba0f532a7fb1d142e1432d2ee87ac48d847e177
Rajasi Rane <rarane@microsoft.com>
2020-04-30 18:24:46 -0700

    Updating license in Package Generator

--- src/tools/Package-All.py
+++ src/tools/Package-All.py
@@ -62,7 +62,21 @@ def write_merged_code(code, merged_file_full_path):
 def insert_copyright_notice(merged_file_full_path, merged_file_name):
     notice = '# --------------------------------------------------------------------------------------------------------------------\n'
     notice += '# <copyright file="' + merged_file_name + '" company="Microsoft">\n'
-    notice += '#   Copyright (c) Microsoft Corporation. All rights reserved.\n'
+    notice += '#   Copyright 2020 Microsoft Corporation\n' \
+              '#\n' \
+              '#   Licensed under the Apache License, Version 2.0 (the "License");\n' \
+              '#   you may not use this file except in compliance with the License.\n' \
+              '#   You may obtain a copy of the License at\n' \
+              '#\n' \
+              '#     http://www.apache.org/licenses/LICENSE-2.0\n' \
+              '#\n' \
+              '#   Unless required by applicable law or agreed to in writing, software\n' \
+              '#   distributed under the License is distributed on an "AS IS" BASIS,\n' \
+              '#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n' \
+              '#   See the License for the specific language governing permissions and\n' \
+              '#   limitations under the License.\n' \
+              '#\n' \
+              '#   Requires Python 2.7+\n'
     notice += '# </copyright>\n'
     notice += '# --------------------------------------------------------------------------------------------------------------------\n\n'
     prepend_content_to_file(notice, merged_file_full_path)
