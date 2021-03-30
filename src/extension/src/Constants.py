55070949c6cdf36d61e71d9f05710d8cb9253cda
Rajasi Rane <rarane@microsoft.com>
2021-03-30 12:56:42 -0700

    Updating extension version for a new release

--- src/extension/src/Constants.py
+++ src/extension/src/Constants.py
@@ -28,7 +28,7 @@ class Constants(object):
                         yield item
 
     # Extension version (todo: move to a different file)
-    EXT_VERSION = "1.6.16"
+    EXT_VERSION = "1.6.17"
 
     # Runtime environments
     TEST = 'Test'
