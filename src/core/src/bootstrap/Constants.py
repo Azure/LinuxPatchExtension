9d832f9f886e0503427532c104bb25b0b242f794
Nikhil M <nikhim@microsoft.com>
2021-08-20 00:05:40 +0530

    version change after arc related commits

--- src/core/src/bootstrap/Constants.py
+++ src/core/src/bootstrap/Constants.py
@@ -30,7 +30,7 @@ class Constants(object):
     UNKNOWN = "Unknown"
 
     # Extension version (todo: move to a different file)
-    EXT_VERSION = "1.6.20"
+    EXT_VERSION = "1.6.21"
 
     # Runtime environments
     TEST = 'Test'
