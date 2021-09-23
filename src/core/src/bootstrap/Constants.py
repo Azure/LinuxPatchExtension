12ee61bd9ba31be3816aafce8a91be2fe2e6d16b
benank <bankiel@microsoft.com>
2021-09-23 11:49:57 -0700

    Update version

--- src/core/src/bootstrap/Constants.py
+++ src/core/src/bootstrap/Constants.py
@@ -30,7 +30,7 @@ class Constants(object):
     UNKNOWN = "Unknown"
 
     # Extension version (todo: move to a different file)
-    EXT_VERSION = "1.6.25"
+    EXT_VERSION = "1.6.26"
 
     # Runtime environments
     TEST = 'Test'
