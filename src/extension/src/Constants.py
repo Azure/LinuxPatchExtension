6af622afb4298805bdf47328d6bc66a785f7166b
Rajasi Rane <56841542+rane-rajasi@users.noreply.github.com>
2021-10-19 17:59:43 -0700

    Disabling auto updates in SUSE (#100)
    * Disabling auto updates in SUSE
    
    * Addressing PR feedback and prepping for  new release

--- src/extension/src/Constants.py
+++ src/extension/src/Constants.py
@@ -28,7 +28,7 @@ class Constants(object):
                         yield item
 
     # Extension version (todo: move to a different file)
-    EXT_VERSION = "1.6.28"
+    EXT_VERSION = "1.6.29"
 
     # Runtime environments
     TEST = 'Test'
