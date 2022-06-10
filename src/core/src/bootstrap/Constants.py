4c7266a9dfa43444b9a9f4d07958cef2593cc2ae
benank <bankiel@microsoft.com>
2022-06-10 10:22:10 -0700

    Changes

--- src/core/src/bootstrap/Constants.py
+++ src/core/src/bootstrap/Constants.py
@@ -126,6 +126,11 @@ class Constants(object):
         DISABLED = "Disabled"
         ENABLED = "Enabled"
 
+    # To separately preserve assessment + auto-assessment state information
+    ASSESSMENT_STATE_FILE = "AssessmentState.json"
+    AUTO_ASSESSMENT_MAXIMUM_DURATION = "PT1H"
+    MIN_AUTO_ASSESSMENT_INTERVAL = "PT6H"   # do not perform auto-assessment if the last assessment happened less than this time interval ago
+
     # wait time after status updates
     WAIT_TIME_AFTER_HEALTHSTORE_STATUS_UPDATE_IN_SECS = 20
     AUTO_ASSESSMENT_MAXIMUM_DURATION = "PT1H"
@@ -258,7 +263,12 @@ class Constants(object):
         Informational = "Informational"
         LogAlways = "LogAlways"
 
-    TELEMETRY_TASK_NAME = "ExtensionCoreLog"
+    # Telemetry Task Names for disambiguation
+    class TelemetryTaskName(EnumBackport):
+        UNKNOWN = "Core.Unknown"                     # function parameter default
+        STARTUP = "Core.Startup"                     # initial value until execution mode is determined
+        EXEC = "Core.Exec"                           # mainline execution triggered from handler
+        AUTO_ASSESSMENT = "Core.AutoAssessment"      # auto-assessment triggered from scheduler
 
     TELEMETRY_AT_AGENT_NOT_COMPATIBLE_ERROR_MSG = "Unsupported older Azure Linux Agent version. To resolve: http://aka.ms/UpdateLinuxAgent"
     TELEMETRY_AT_AGENT_COMPATIBLE_MSG = "Minimum Azure Linux Agent version prerequisite met"
