25e1860473af9f506ba1b82c2311ddca0f017431
benank <8016617+benank@users.noreply.github.com>
2022-07-25 11:32:52 -0700

    Update version to v1.6.41 (#157)

--- src/extension/src/Constants.py
+++ src/extension/src/Constants.py
@@ -28,7 +28,7 @@ class Constants(object):
                         yield item
 
     # Extension version (todo: move to a different file)
-    EXT_VERSION = "1.6.40"
+    EXT_VERSION = "1.6.41"
 
     # Runtime environments
     TEST = 'Test'
