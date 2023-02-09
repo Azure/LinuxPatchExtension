9b38e30b5ed624efd4076f2e686ff4f6f3c1fdfa
Rajasi Rane <rarane@microsoft.com>
2023-02-09 14:14:13 -0800

    Updating extension version to 1.6.44

--- src/core/src/bootstrap/Constants.py
+++ src/core/src/bootstrap/Constants.py
@@ -30,7 +30,7 @@ class Constants(object):
     UNKNOWN = "Unknown"
 
     # Extension version (todo: move to a different file)
-    EXT_VERSION = "1.6.43"
+    EXT_VERSION = "1.6.44"
 
     # Runtime environments
     TEST = 'Test'
