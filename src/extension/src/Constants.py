8d8c33599fdfa72651a2f788b36ea7b23d08cce3
feng-j678 <127874208+feng-j678@users.noreply.github.com>
2024-04-01 14:30:06 -0700

    update extension to 1.6.53 (#255)

--- src/extension/src/Constants.py
+++ src/extension/src/Constants.py
@@ -28,7 +28,7 @@ class Constants(object):
                         yield item
 
     # Extension version (todo: move to a different file)
-    EXT_VERSION = "1.6.52"
+    EXT_VERSION = "1.6.53"
 
     # Runtime environments
     TEST = 'Test'
