25f45d426dadb6a45ab21829e292d3c78aaea156
Yashna Parikh <yashnaparikh@microsoft.com>
2026-08-06 10:40:25 -0400

    Test

--- src/extension/tests/Test_ActionHandler.py
+++ src/extension/tests/Test_ActionHandler.py
@@ -310,6 +310,7 @@ class TestActionHandler(unittest.TestCase):
 
         # Re-init TelemetryWriter since the env var for compatibility is only checked on init
         os.getenv = mock_os_getenv
+        #test
         self.runtime.telemetry_writer = TelemetryWriter(self.runtime.logger, self.runtime.env_layer, self.runtime.credential_sanitizer)
         self.action_handler.telemetry_writer = self.runtime.telemetry_writer
 
